b75021c5e1bd8ff8df641d8bad4be8448f52bd62
Xhensila Hoxha <xhensila.hoxha@fau.de>
2024-12-03 13:34:25 +0100

    Script for testing

--- project/tests.sh
+++ project/tests.sh
@@ -5,4 +5,4 @@ set -e # Initiate immediate exit
 echo "Waiting for tests checks..."
 pytest tests.py
 
-echo "All checks passed. The pipeline processed end-to-end data successfuly!"
+echo "All checks passed! The pipeline processed end-to-end data successfuly!"
