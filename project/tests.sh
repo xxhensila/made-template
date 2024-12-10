f203afa5c10d0a64cea05a710fd926d4ca39cef2
xhensilahoxha <116840506+xxhensila@users.noreply.github.com>
2024-12-10 14:53:31 +0100

    Update tests.py path

--- project/tests.sh
+++ project/tests.sh
@@ -3,6 +3,6 @@
 set -e # Initiate immediate exit
 
 echo "Waiting for tests checks..."
-pytest tests.py
+pytest ./project/tests.py
 
 echo "All checks passed! The pipeline processed end-to-end data successfuly!"
